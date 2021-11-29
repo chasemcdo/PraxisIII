f5b7c9b24049c9725d1df712f3bbc9a487eb1646
BuildTools <chasemcdougall@hotmail.com>
2021-11-29 16:39:03 -0500

    Updated code and sample GPS data

--- GPS_Tracker/code.py
+++ GPS_Tracker/code.py
@@ -9,40 +9,43 @@ write_pin = digitalio.DigitalInOut(board.GP0)
 write_pin.direction = digitalio.Direction.INPUT
 write_pin.pull = digitalio.Pull.UP
 
-# Sets up LED for activity indicator
+# Setup Activation LED
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-# Reads current contents of data.txt for future use
+# Read current data from file
 data_filename = 'data.txt'
 f = open(data_filename, 'r')
 string = f.read()
 f.close()
 
-# initiates gps_module
+# Setup GPS module
 gps_module = busio.UART(tx=board.GP4, rx=board.GP5, baudrate=9600)
 gps = adafruit_gps.GPS(gps_module, debug=False)
 
-# Turn on LED
+# Turn on LED and wait 5 seconds to give user time to unpress button
 led.value = 1
-# Wait so that user can unpress button
 time.sleep(5)
 
-last_print = time.monotonic() # Records current time
+# Initialize current time for spacing writes
+last_print = time.monotonic()
 
-# Opens file for writing, rewrites old data, and starts loop to gather fresh data
+# Open data.txt for writing, write old data and begin loop
 f = open(data_filename, 'w')
 f.write(string)
 while True:
     current = time.monotonic()
-    gps.update() # updates gps info
-    
-    if (current - last_print >= 3):
+    gps.update()
+
+    if (current - last_print >= 5):
         last_print = current
-        temp_str = f"\n{gps.timestamp_utc[3] + gps.timestamp_utc[4]/60},{gps.latitude},{gps.longitude}"
-        f.write(temp_str)
-        #print(temp_str)
-        
+        if (gps.timestamp_utc):
+            temp_str = f"\n{gps.timestamp_utc[3] + gps.timestamp_utc[4]/60},{gps.latitude},{gps.longitude}"
+            f.write(temp_str)
+            print(temp_str)
+        else:
+            print('No GPS Info Yet')
+
     if (not write_pin.value):
         f.close()
         break
